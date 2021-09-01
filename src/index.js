6f97ebdfad12a910bbf40425795cbaeacffd5807
Guilherme <guiborgesilva@outlook.com>
2021-09-01 11:06:53 -0300

    cors resolve

--- src/index.js
+++ src/index.js
@@ -11,21 +11,12 @@ const app = express()
 /*
 * Database setup
 */
-
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  useNewUrlParser: true
 })
 
+app.use(cors({ origin: '*' }))
 app.use(express.json())
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-  app.use(cors())
-  next()
-})
-
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.use('/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')))
