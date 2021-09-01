8423c807c1d0d8cb0d7433b6e9b75c0ece5e90fe
Guilherme <guiborgesilva@outlook.com>
2021-09-01 15:42:40 -0300

    Teste cors

--- src/index.js
+++ src/index.js
@@ -15,7 +15,17 @@ mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true
 })
 
-app.use(cors({ origin: '*', methods: 'GET, POSTS, DELETE' }))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    return res.status(200).send({})
+  }
+
+  next()
+})
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
