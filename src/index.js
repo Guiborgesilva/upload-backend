a5f499557b727cfa09620c7080af612fa4a0ae65
Guilherme <guiborgesilva@outlook.com>
2021-09-01 10:38:27 -0300

    Resolve problema do cors

--- src/index.js
+++ src/index.js
@@ -11,12 +11,17 @@ const app = express()
 /*
 * Database setup
 */
+
+app.use((req, res, next) => {
+  app.use(cors())
+  next()
+})
+
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
